4c5e002e883013fb2a4a126ad7b2878549e0e738
Enrick <enrickdaltro@gmail.com>
2020-02-27 11:26:54 -0300

    styling signin and signup page

--- src/pages/SignUp/index.js
+++ src/pages/SignUp/index.js
@@ -1,7 +1,21 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
-// import { Container } from './styles';
+import logo from '~/assets/1581704670044-attachment.svg';
 
-export default function SignUp() {
-  return <div />;
+export default function SignUn() {
+  return (
+    <>
+      <img src={logo} alt="GoBarber" />
+
+      <form>
+        <input type="text" placeholder="Nome Completo" />
+        <input type="email" placeholder="Seu email" />
+        <input type="password" placeholder="Sua senha" />
+
+        <button type="submit">Criar Conta</button>
+        <Link to="/">Já tenho login</Link>
+      </form>
+    </>
+  );
 }
