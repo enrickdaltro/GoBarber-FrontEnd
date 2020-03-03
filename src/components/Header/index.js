f1851fe79d3c5ce05c20df1da46e925ae4693cfc
Enrick <enrickdaltro@gmail.com>
2020-03-02 23:01:05 -0300

    add: listing appointments

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -28,8 +28,9 @@ export default function Header() {
             </div>
             <img
               src={
-                profile.avatar.url ||
-                'https://api.adorable.io/avatars/50/abott@adorable.pngCopy"'
+                profile.avatar
+                  ? profile.avatar.url
+                  : 'https://api.adorable.io/avatars/50/abott@adorable.pngCopy'
               }
               alt="Ze Silva"
             />
